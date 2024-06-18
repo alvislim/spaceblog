af3e37a9f9d82766fc2936a31821f5e507f26617
alvislim <luvhatehero1@gmail.com>
2024-06-18 13:23:53 +0800

    updated card article style and responsiveness

--- src/component/article/index.tsx
+++ src/component/article/index.tsx
@@ -1,7 +1,7 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
 import Box from "@mui/material/Box";
 
 type Props = {
@@ -16,14 +16,30 @@ const Article = (props: Props) => {
   const { img, title, summary, publishedDate, id } = props;
   return (
     <Card sx={{ maxWidth: 900 }} key={id}>
-      <Box display='flex' gap={2}>
-        <img alt='article-img' height='auto' width='300' src={img} />
+      <Box
+        display='flex'
+        gap={2}
+        sx={{ flexDirection: { xs: "column", md: "row" } }}>
+        <Box p={2}>
+          <CardMedia
+            title='article-img'
+            image={img}
+            sx={{
+              width: { xs: "100%", md: 350 },
+              height: { xs: 300, md: "100%" },
+              mid: "auto",
+            }}
+          />
+        </Box>
         <CardContent>
-          <Box display='flex' flexDirection='column' gap={4}>
-            <Typography variant='h5' component='div'>
+          <Box display='flex' flexDirection='column' gap={4} alignItems='start'>
+            <Typography variant='h5' component='div' sx={{ textAlign: "left" }}>
               {title}
             </Typography>
-            <Typography variant='body2' color='text.secondary'>
+            <Typography
+              variant='body2'
+              color='text.secondary'
+              sx={{ textAlign: "left" }}>
               {summary}
             </Typography>
             <Typography>{publishedDate}</Typography>
